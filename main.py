26da4687d820acb24ff9287e1a58850e82e16011
louis <waslouis1111@gmail.com>
2021-09-15 11:20:48 +0800

    change main2

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import uvicorn
+# import uvicorn
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def root():
+#     a = "a"
+#     b = "b" + a
+#     return {"hello world": b}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=5000)
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+
 
 @app.get("/")
-def root():
-    a = "a"
-    b = "b" + a
-    return {"hello world": b}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+def home():
+    return {"message":"Hello TutLinks.com"}
