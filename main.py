a5a46b179c2daa5a98f727bc5251b229f556b5e0
louis <waslouis1111@gmail.com>
2021-09-15 23:42:27 +0800

    url return

--- main.py
+++ main.py
@@ -15,9 +15,12 @@
 #     uvicorn.run(app, host="0.0.0.0", port=5000)
 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import requests
 from starlette.responses import FileResponse
-
+from fastapi.responses import StreamingResponse
+from moviepy.editor import *
+from pytube import YouTube
 
 app = FastAPI()
 
@@ -27,8 +30,52 @@ app = FastAPI()
 @app.get("/")
 def home():
     return FileResponse('./who.3gpp',filename="who.3gpp")
+    
+@app.get("/getVidoName/{token}")
+async def getVidoName(token:str):
+    # yt = YouTube(url)
+    # return yt.title
+    url = 'https://www.youtube.com/watch?v='+token
+    yt = YouTube(url)
+    return yt.title
+
+@app.get("/downloadVideo/{token}")
+async def getVidoName(token:str):
+    url = 'https://www.youtube.com/watch?v='+token
+    yt = YouTube(url)
+    # yt.streams.first().download()
+    tube_url = yt.streams.first().url
+
+    return tube_url
+
+@app.get("/mp3")
+def mp3():
+    video = VideoFileClip('./who.3gpp')
+    video.audio.write_audiofile('test.mp3')
+    return FileResponse('./test.mp3',filename="test.mp3")
 
 @app.get("/music")
-def home():
+def music():
     with open('./who.3gpp', 'r') as f:
         return f.read()
+
+@app.get('/other/{token:str}')
+async def other_api(token: str, req: Request):
+    """透传 API"""
+    url = 'https://www.youtube.com/watch?v='+token
+    yt = YouTube(url)
+    # yt.streams.first().download()
+    tube_url = yt.streams.first().url
+
+    body = bytes(await req.body()) or None
+    r = requests.request(
+        req.method, tube_url,
+        headers={
+            'Cookie': req.headers.get('cookie') or '',
+            'Content-Type': req.headers.get('Content-Type')},
+        params=req.query_params, data=body, stream=True,
+        allow_redirects=False)
+
+    h = dict(r.headers)
+    h.pop('Content-Length', None)
+    return StreamingResponse(r.raw, headers=h, status_code=r.status_code)
